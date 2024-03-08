018335ba2ad91811252b7c797c4441c2801e947e
Muhammad Ahsan Naeem <ahsannaeem480@gmail.com>
2024-03-08 11:55:43 +0500

    Sample Content Removed from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Hi i am Ahmad Naeem and I am a full stack web developer</p>
-        <p>This line add in dev 1 branch </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello</h1>
     </>
   )
 }
